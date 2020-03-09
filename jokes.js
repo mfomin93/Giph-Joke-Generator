3f5288159ee530648a1409e71d390a83d9011123
mfomin93 <mfomin93@gmail.com>
2020-03-08 20:47:53 -0400

    js and html changes

--- jokes.js
+++ jokes.js
@@ -1,22 +1,30 @@
-function init() {
-    document.getElementById("jokeBtn").addEventListener("click", ev => {
-      $("#jokeBtn").on("click", function() {
-          ev.preventDefault();
-          
-          var queryURL = "https://joke3.p.rapidapi.com/v1/joke";
-          url = url.concat(str);
-          fetch(url)
-         .then(response => response.json()
-         .then(response => {
-      var jokeContent = document.createElement('jokeContent');
-      jokeContent.appendChild(div);
-      var jokeOut = document.querySelector(".jokeOut");
-      jokeOut.insertAdjacentElement('afterbegin', jokeContent);
-      document.querySelector('#jokeBtn').value = '';       
-         })
+// Mark's API
+
+    // Event listener for our Joke button
+    $("#jokeBtn").on("click", function() {
+        // Storing our giphy API URL for a random cat image
+        var jokeURL = 'https://joke3.p.rapidapi.com/v1/joke'
+        // Perfoming an AJAX GET request to our queryURL
+        $.ajax({
+          url: jokeURL,
+    method: "GET",
+    headers: {
+  "x-rapidapi-host": "joke3.p.rapidapi.com",
+  "x-rapidapi-key": "7633fefb42msh97b1ac4a8018328p11e565jsnb20273420f07"
+        }})
+        // After the data from the AJAX request comes back
+          .then(function(response) {
+            console.log(response);
+          // Saving the image_original_url property
+            var imageUrl = response.content;
+            // Creating and storing a p tag to store the joke text
+            var jokeText = $("<p>");
+            // Setting the jokeText src attribute to imageUrl
+            jokeText.prepend(imageUrl);
+           
+            // Prepending the jokeText to the images div
+            $("#joke-el").prepend(jokeText);
+          });
+      });
   
-         .catch(err=>{
-         }));
-         });
-        });
-    }
+  // Mark's API
