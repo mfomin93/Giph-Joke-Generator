7de5afe98c764330dae4b3b594ab71290c933cf9
Vannak <vannaksuos@gmail.com>
2020-03-09 20:06:19 -0400

    finalize

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 var APIKEY = "o58fMMxvAkc7hySu0MF9E1duUL47viNk";
-
 function init() {
   $("#go").on("click", function (ev) {
     ev.preventDefault();
     var userSearch = $("#search").val();
     var queryURL = `https://api.giphy.com/v1/gifs/random?api_key=${APIKEY}&limit=1&tag=${userSearch}`;
-
+    // var queryURL = "https://api.giphy.com/v1/gifs/random?api_key="+APIKEY+"&limit=1&tag="+userSearch;   (ECMAscript 6)
     $.ajax({
       url: queryURL,
       method: "GET"
     })
       .then((content) => {
-
         var giph = document.createElement('giph');
         var img = document.createElement('img');
         img.src = content.data.images.downsized.url;
@@ -19,9 +17,7 @@ function init() {
         var out = document.querySelector('.out');
         out.insertAdjacentElement('afterbegin', giph);
         $('#search').val('');
-
       })
   })
 };
-
 init();
