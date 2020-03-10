2a8cb78e2113450e48d967ffe2699117ffabab32
chris <chris37x@live.com>
2020-03-09 20:09:07 -0400

    added js/html files

--- jokes.js
+++ jokes.js
@@ -1,28 +1,24 @@
-function generateJoke(){
-    $("#jokeBtn").on("click", function(ev) {
-      ev.preventDefault();  
-      var jokeURL = 'https://joke3.p.rapidapi.com/v1/joke';
-      
-      $.ajax({
+function generateJoke() {
+    $("#jokeBtn").on("click", function (ev) {
+    ev.preventDefault();
+    var jokeURL = 'https://joke3.p.rapidapi.com/v1/joke';
+
+        $.ajax({
         url: jokeURL,
-    method: "GET",
-    headers: {
-    "x-rapidapi-host": "joke3.p.rapidapi.com",
-    "x-rapidapi-key": "7633fefb42msh97b1ac4a8018328p11e565jsnb20273420f07"
-      }})
-     
-        .then(function(response) {
-          console.log(response);
-       
-          var imageUrl = response.content;
-        
-          var jokeText = $("<ul>");
-       
-          jokeText.prepend(imageUrl);
-    
-          $("#joke-el").prepend(jokeText);
+        method: "GET",
+        headers: {
+        "x-rapidapi-host": "joke3.p.rapidapi.com",
+        "x-rapidapi-key": "7633fefb42msh97b1ac4a8018328p11e565jsnb20273420f07"
+            }
+        })
+
+        .then(function (response) {
+        var imageUrl = response.content;
+        var jokeText = $("<ul>");
+        jokeText.prepend(imageUrl);
+        $("#joke-el").prepend(jokeText);
         });
     });
-  }
-  
-  generateJoke();
+}
+
+generateJoke();
